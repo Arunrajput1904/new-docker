ce5282bb7a8dfa3a7b750eeb2ccabd5f986729d1
Arun <arunrajput16177@gmail.com>
2026-03-31 00:26:03 +0530

    target file configure

---
+++ SchoolManagementSystem/target/classes/data.sql
@@ -0,0 +1,52 @@
+INSERT INTO professor_entity (name)
+VALUES
+('sachin'),
+('vikram');
+
+
+
+INSERT INTO student_entity (name)
+VALUES
+('harsh'),
+('aman'),
+('rohit'),
+('kartik'),
+('sachin'),
+('vikram');
+
+INSERT INTO subject_entity (title)
+VALUES
+('Math'),
+('Physics'),
+('Chemistry'),
+('Biology'),
+('Computer Science'),
+('English'),
+('History'),
+('Geography'),
+('Economics'),
+('Statistics');
+
+
+
+INSERT INTO admin_entity (fees)
+VALUES
+(1000),
+(1500),
+(2000),
+(2500),
+(4000),
+(3000);
+
+INSERT INTO EMPLOYEE (FNAME, LNAME, EMAIL, DEPT, SALARY)
+VALUES
+('ARUN',  'KUMAR',  'ARUNRAJPUT161772@GMAIL.COM', 'ENGINEER', 10000),
+('AMAN',  'KUMAR',  'AMANRAJPUT16177@GMAIL.COM', 'TEACHER',  40000),
+('RAHUL', 'SHARMA', 'RAHUL.SHARMA@GMAIL.COM',    'ENGINEER', 35000),
+('ROHIT', 'VERMA',  'ROHIT.VERMA@GMAIL.COM',     'HR',       28000),
+('NEHA',  'SINGH',  'NEHA.SINGH@GMAIL.COM',      'FINANCE',  45000),
+('PRIYA', 'GUPTA',  'PRIYA.GUPTA@GMAIL.COM',     'MARKETING',30000),
+('VIKAS', 'MEENA',  'VIKAS.MEENA@GMAIL.COM',     'ENGINEER', 50000),
+('SONAM', 'JAIN',   'SONAM.JAIN@GMAIL.COM',      'HR',       32000),
+('ANKIT', 'YADAV',  'ANKIT.YADAV@GMAIL.COM',     'SALES',    27000),
+('POOJA', 'MALIK',  'POOJA.MALIK@GMAIL.COM',     'TEACHER',  38000);
